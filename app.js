09a320f7db563cb56a90c6e940b05e4280cde91f
midasDev777 <princehans2023@gmail.com>
2019-09-28 20:03:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ const app = express();
 // app.options('http://localhost:4200', cors);
 // uncomment after placing your favicon in /public
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'https://cofluencer.netlify.com/');
   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
