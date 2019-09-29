0b1b027343178547026a6be868f0314d36b22e71
midasDev777 <princehans2023@gmail.com>
2019-09-29 08:40:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const app = express();
 // uncomment after placing your favicon in /public
 app.use((req, res, next) => {
   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://cofluencer.netlify.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
